56f05a1605e38184b352967aac8a890fa8890178
suraj8853 <surajpai8853@gmail.com>
2026-06-10 13:59:57 +0000

    fix(iam): restore EKS OIDC provider resource

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -7,6 +7,15 @@ resource "aws_iam_openid_connect_provider" "github" {
   }
 }
 
+resource "aws_iam_openid_connect_provider" "eks" {
+  url             = var.eks_oidc_issuer_url
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+  tags = {
+    Name = "${var.project}-${var.env}-eks-oidc"
+  }
+}
+
 
 
 locals {
